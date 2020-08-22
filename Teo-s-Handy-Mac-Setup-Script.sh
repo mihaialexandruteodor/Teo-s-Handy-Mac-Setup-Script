0ceda243dab2b363be5a36bad6e2ba71b22a3b2f
mihaiteodor97 <mihaialexandruteodor@gmail.com>
2020-08-22 20:51:23 +0300

    * Homebrew install

---
+++ Teo-s-Handy-Mac-Setup-Script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+echo -e "\n"
+echo "Teo's Handy Mac Setup Script.sh"
+echo -e "Mihai Alexandru Teodor ©2020\n"
+
+echo -e "################################################################\n\n"
+
+echo -e "I'll guide you all the way, remember that shell execution can be interrupted by pressing Ctrl+Z\n"
+
+echo "First we'll install Homebrew, the Mac package manager"
+echo -e "You'll need to type in your password below\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
